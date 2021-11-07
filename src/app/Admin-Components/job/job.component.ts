e3d41e98f4c4d02aead48591fc7e8525ad7ce02c
MK167 <58882363+MK167@users.noreply.github.com>
2021-11-07 09:09:32 +0200

    Update Services

--- src/app/Admin-Components/job/job.component.ts
+++ src/app/Admin-Components/job/job.component.ts
@@ -1,15 +1,85 @@
-import { Component, OnInit } from '@angular/core';
+import { JobService } from './../../Services/Admin-Services/Job/job.service';
+import { IJobDTO } from './../../Models/IJobDTO';
+import { SelectionModel } from '@angular/cdk/collections';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { UserData } from 'app/table-list/table-list.component';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { AfterViewInit } from '@angular/core';
+import { Subject } from 'rxjs';
+
+/**
+ * @title Data table with sorting, pagination, and filtering.
+ */
+
 
 @Component({
   selector: 'app-job',
   templateUrl: './job.component.html',
   styleUrls: ['./job.component.css']
 })
-export class JobComponent implements OnInit {
+export class JobComponent implements OnInit, AfterViewInit {
+  dtTrigger: Subject<any> = new Subject();
+  
+  Jobs : IJobDTO;
+  displayedColumns: string[] = ['jobID', 'jobName', 'Actions'];
+  dataSource: MatTableDataSource<IJobDTO>;
+  selection = new SelectionModel<IJobDTO>(true, []);
 
-  constructor() { }
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected ===
+    numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    if (this.isAllSelected()) {
+      this.selection.clear();
+      return;
+    }
+
+    this.selection.select(...this.dataSource.data);
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    }
+
+    applyFilter(event: Event) {
+      const filterValue = (event.target as HTMLInputElement).value;
+      this.dataSource.filter = filterValue.trim().toLowerCase();
+  
+      if (this.dataSource.paginator) {
+        this.dataSource.paginator.firstPage();
+      }
+    }
+
+  constructor(
+    private JobService: JobService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
+    this.LoadJobs();
+  }
+
+  LoadJobs(){
+    this.JobService.GetAllJob().subscribe((data: IJobDTO) => {
+      this.Jobs = data;
+      console.log(this.Jobs);
+   });
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,9 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  //To hit API on localhost use localhost link 
+  //If you used on production change the baseUrl in environment.prod.ts
+  baseUrl: 'https://localhost:44370/api/'
 };
